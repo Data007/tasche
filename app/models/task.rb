94b6a43bd4108e8df88c6f248a48fdf466a4f92d
Bryan <lordbyron@theswift.(none)>
2009-02-17 12:50:50 -0500

    removing old stuf

--- app/models/task.rb
+++
@@ -1,7 +0,0 @@
-class Task < ActiveRecord::Base
-  has_many                :tags, :as => :taggable
-  has_and_belongs_to_many :projects
-  has_and_belongs_to_many :users
-  
-  named_scope :order_by_priority, :order => "priority ASC"
-end
--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_and_belongs_to_many :projects
-  has_and_belongs_to_many :tasks
-  has_many                :tags, :as => :taggable
-  
-  named_scope :active,    :conditions => {:is_active => true}
-  named_scope :inactive,  :conditions => {:is_active => false}
-end
